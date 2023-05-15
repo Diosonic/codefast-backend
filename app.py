52cc19edc575685b9b4874442d4d3b9f1a3d8186
Yago <peixinhoyago@gmail.com>
2023-05-14 21:35:42 -0300

    :sparkles: feat: Remove unsuned variables

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
 from flask_migrate import Migrate
-from flask_cors import CORS
 from flask_mysqldb import MySQL
 
 
